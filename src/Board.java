a30b70c876ca21c5d83f6833fb314e6cc2a600fc
Kim Bellander <kim.bellander@gmail.com>
2024-10-22 11:18:19 +0200

    Messed around with testing the arrays.

--- src/Board.java
+++ src/Board.java
@@ -1,42 +1,74 @@
+import java.util.Arrays;
+
 public class Board {
 
-   /* int[] rowCollection;
-    int[] columnCollection;*/
-    private int numberOfHorizontalSquares = 7;
-    private int numberOfVerticalSquares = 10;
-    int[][] board = new int[numberOfHorizontalSquares][numberOfVerticalSquares];
-   /* int[][] boardCollection;
+    private int rowCollection = 7;
+    private int columnCollection = 10;
+    /*private char[] rowTest;
+    private char[] columnTest;
+    private String [][] boardTest;*/
+    int[][] boardCollection = new int[rowCollection][columnCollection];
+
+   /*
     int[][] bombCollection;*/
 
     public Board() {
 
     }
 
-    public void presentBoard() {
-        for(int row = 0; row < numberOfVerticalSquares; row++) {
+
+/* SAVE FOR LATER PERHAPS
+    public Board() {
+        this.rowTest = new char[]{'a', 'b', 'c', 'd', 'e'};
+        this.columnTest = new char[]{'1', '2', '3', '4', '5'};
+
+
+
+    }
+
+    public void createBoard() {
+        String[] temporaryRow = new String[columnTest.length];
+        for(int i = 0; i < rowTest.length; i++) {
+            for(int j = 0; j < columnTest.length; j++) {
+                temporaryRow[j] = "" + rowTest[i] + columnTest[j];
+//                System.out.println("" + rowTest[i] + columnTest[j]);
+            }
+            boardTest[i] = temporaryRow;
+        }
+        System.out.println((Arrays.deepToString(boardTest)));
+
+    }
+
+ */
+
+
+
+    public void printBoard() {
+        for(int row = 0; row < columnCollection; row++) {
             System.out.print("   ");
-            for(int i = 0; i < numberOfHorizontalSquares; i++) {
+            for(int i = 0; i < rowCollection; i++) {
 
                 System.out.print("+----");
             }
             System.out.println("+");
-            System.out.print(numberOfVerticalSquares-row+"  ");
+            System.out.print(columnCollection -row+"  ");
 
-            for(int column = 1; column < numberOfHorizontalSquares+1; column++) {
+            for(int column = 1; column < rowCollection +1; column++) {
                 System.out.print("|    ");
+//                System.out.print("| " + row + "," + (column - 1) + " ");
             }
             System.out.println("|");
         }
 
         System.out.print("   ");
-        for(int i = 0; i<numberOfHorizontalSquares; i++) {
+        for(int i = 0; i< rowCollection; i++) {
             System.out.print("+----");
         }
         System.out.println("+");
 
         System.out.print("      ");
-        for(int i =0; i<numberOfHorizontalSquares; i++) {
-            System.out.print(numberOfVerticalSquares-numberOfVerticalSquares+i+1+"    ");
+        for(int i = 0; i< rowCollection; i++) {
+            System.out.print(columnCollection - columnCollection +i+1+"    ");
         }
     }
 
@@ -61,19 +93,23 @@ public class Board {
         return false;
     }
 
-    public int getNumberOfHorizontalSquares() {
-        return numberOfHorizontalSquares;
+    public boolean choosePosition(int userInputRow, int userInputColumn) {
+        return false;
     }
 
-    public void setNumberOfHorizontalSquares(int numberOfHorizontalSquares) {
-        this.numberOfHorizontalSquares = numberOfHorizontalSquares;
+    public int getRowCollection() {
+        return rowCollection;
     }
 
-    public int getNumberOfVerticalSquares() {
-        return numberOfVerticalSquares;
+    public void setRowCollection(int rowCollection) {
+        this.rowCollection = rowCollection;
     }
 
-    public void setNumberOfVerticalSquares(int numberOfVerticalSquares) {
-        this.numberOfVerticalSquares = numberOfVerticalSquares;
+    public int getColumnCollection() {
+        return columnCollection;
+    }
+
+    public void setColumnCollection(int columnCollection) {
+        this.columnCollection = columnCollection;
     }
 }
