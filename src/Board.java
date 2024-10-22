fac96c114814753f9c77a4e2551fca48ac50efde
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-22 11:50:44 +0200

    Declare method that creates a board internally

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Board {
+    /**
+     * Minesweeper rows.
+     */
+    final private ArrayList<Character> rowCollection = new ArrayList<>();
 
-   /* int[] rowCollection;
-    int[] columnCollection;*/
-    private int numberOfHorizontalSquares = 7;
-    private int numberOfVerticalSquares = 10;
-    int[][] board = new int[numberOfHorizontalSquares][numberOfVerticalSquares];
-   /* int[][] boardCollection;
-    int[][] bombCollection;*/
+    /**
+     * Minesweeper columns.
+     */
+    final private ArrayList<Integer> columnCollection = new ArrayList<>();
 
+    /**
+     * Minesweeper matrices representing board.
+     */
+    final private ArrayList<ArrayList<String>> boardCollection = new ArrayList<ArrayList<String>>();
+
+    /**
+     * Creates an instance of this class.
+     */
     public Board() {
+        rowCollection.addAll(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
+        columnCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26));
+    }
 
+    /**
+     * Creates a board based on difficulty.
+     * @param rows - Number of rows based on difficulty.
+     * @param columns - Number of columns based on difficulty.
+     */
+    public void createBoard(int rows, int columns) {
+        ArrayList<String> tempRow = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                tempRow.add("" + rowCollection.get(i) + columnCollection.get(j));
+            }
+            boardCollection.add(new ArrayList<>(tempRow));
+            tempRow.clear();
+        }
+        System.out.println(boardCollection.toString());
     }
 
     public void presentBoard() {
@@ -76,4 +106,5 @@ public class Board {
     public void setNumberOfVerticalSquares(int numberOfVerticalSquares) {
         this.numberOfVerticalSquares = numberOfVerticalSquares;
     }
+
 }
