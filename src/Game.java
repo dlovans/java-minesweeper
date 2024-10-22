981fce19a9dda99504e8d58128efebfb04ae45cb
DJmoos3 <isaac.strandh@gmail.com>
2024-10-22 09:38:13 +0200

    Initial variables and methods

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Game {
+    /*Player player;
+    Board board;
+    */
+    Scanner scanner = new Scanner(System.in);
+    int userInput;
+
+    public void gameLoop(){
+
+    }
+
+    public void playerInput(){
+
+    }
+}
