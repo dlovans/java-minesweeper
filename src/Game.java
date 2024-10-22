6a11eb948cc7e4357e657715e5c002a7933d8c70
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-22 09:32:00 +0200

    Declare Game class

---
+++ src/Game.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * Represents game of minesweeper.
+ */
+public class Game {
+    /**
+     * Player playing minesweeper.
+     */
+    Player player;
+
+    /**
+     * Board of minesweeper.
+     */
+    Board board;
+
+    /**
+     * Reads input from stdout.
+     */
+    Scanner scanner = new Scanner(System.in);
+
+    /**
+     * User menu selection.
+     */
+    int userInput;
+
+    /**
+     * Creates a game instance.
+     * @param player - Player playing this game.
+     * @param board - The board of minesweeper.
+     */
+    public Game(Player player, Board board) {
+        this.player = player;
+        this.board = board;
+    }
+}
